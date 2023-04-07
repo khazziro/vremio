001ac624c61fa2baa4d7f8692693de7e989aeaae
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-07 18:02:40 +0300

    Revert "Added mock-up for the Weather API"
    This reverts commit 6e48963a9f162c11f023136a9500eab1a832b4b2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import Weather from "./api/weather";
-
 function App() {
-  return (
-    <div className="h-screen w-full relative bg-gray-500 text-white before:content-[''] before:bg-[url('/src/assets/react.svg')] before:bg-no-repeat before:bg-cover before:bg-center before:absolute before:w-full before:h-full before:top-0 before:left-0">
-      <Weather />
-    </div>
-  );
+  return <div className="App text-center text-xl">Test</div>;
 }
 
 export default App;
