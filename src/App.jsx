663fda1e45e59fc227c1e8f2c3fcc85a1a14affe
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-08 16:18:42 +0300

    Cleaned up code and changed background image

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Weather from "./components/weather";
 
 function App() {
   return (
-    <div className="w-full h-screen relative bg-neutral-500 text-white before:content-[''] before:bg-[url('/src/assets/square_background.jpg')] before:absolute  before:w-full before:h-full before:top-0 before:left-0  before:bg-no-repeat before:bg-cover before:bg-center">
+    <div className="w-full h-screen relative bg-neutral-500 text-white before:content-[''] before:bg-[url('/src/assets/background_image_1.jpg')] before:absolute  before:w-full before:h-full before:top-0 before:left-0  before:bg-no-repeat before:bg-cover before:bg-center">
       <Weather />
     </div>
   );
