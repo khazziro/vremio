1750ed84eb81422114386d9ec4c852e68efbc009
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-08 16:12:54 +0300

    Added location search functionality with axios API call and dynamic rendering of weather information

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,33 +1,68 @@
-import React from "react";
-// import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Weather = () => {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=Oradea&appid=4775d45af32f6ca2bc8c28c6e6d79cb0`;
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=4775d45af32f6ca2bc8c28c6e6d79cb0`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((r) => {
+        setData(r.data);
+        console.log(r.data);
+      });
+      setLocation("");
+    }
+  };
 
   return (
-    <div className="container relative">
-      <div className="top">
-        <div className="location">
-          <p className="text-2xl">Oradea</p>
+    <div className="flex flex-col justify-between container max-w-screen-md h-full relative m-auto p-4 ">
+      <div className="search contents text-center">
+        <input
+          className="py-3 px-5 rounded-3xl bg-white bg-opacity-20 placeholder:text-white"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter Location"
+        />
+      </div>
+      <div className="w-full my-4 mx-auto">
+        <div>
+          <p className="text-2xl">{data.name}</p>
         </div>
-        <div className="temp">
-          <h2 className="text-7xl">21 C</h2>
+        <div>
+          {data.main ? (
+            <h2 className="text-7xl font-bold">{data.main.temp.toFixed()}°C</h2>
+          ) : null}
         </div>
-        <div className="description">
-          <p className="text-2xl">Cloudy</p>
+        <div className="relative right-[-90%] origin-top-left -rotate-90">
+          {data.weather ? (
+            <p className="text-2xl">{data.weather[0].description}</p>
+          ) : null}
         </div>
       </div>
-      <div className="bottom">
-        <div className="feels">
-          <p className="text-2xl">22 C</p>
+      {data.name !== undefined && (
+        <div className="flex w-full justify-evenly text-center my-4 mb-0 mx-auto p-4 rounded-xl bg-white bg-opacity-20">
+          <div className="text-xl mr-2">
+            {data.main ? (
+              <p className="font-bold">{data.main.feels_like.toFixed()}°C</p>
+            ) : null}
+            <p className="text-base">Feels Like</p>
+          </div>
+          <div className="text-xl mr-2">
+            {data.main ? (
+              <p className="font-bold">{data.main.humidity}%</p>
+            ) : null}
+            <p className="text-base">Humidity</p>
+          </div>
+          <div className="text-xl">
+            {data.wind ? <p className="font-bold">{data.wind.speed}</p> : null}
+            <p className="text-base">Wind Speed</p>
+          </div>
         </div>
-        <div className="humidity">
-          <p className="text-2xl">20%</p>
-        </div>
-        <div className="wind">
-          <p className="text-2xl">1.2 KMPH</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
